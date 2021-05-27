2c88f0f51db487cdb70a72c6e726f4bb630eebd4
ragq <ragq1991@gmail.com>
2021-05-27 16:33:06 +0600

    Fix "if __name__ == '__main__':"

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-import hashlib
+WIKI = 'https://en.wikipedia.org/wiki/'
 
-def gen(path):
-    with open(path, "r") as data:
-        end = sum(1 for line in data)
-        data.seek(0)
-        for i in range(0, end):
-            if i > end:
-                raise StopIteration
-            hash = hashlib.md5(data.read(i).encode())
-            yield hash.hexdigest()
+import json
 
+class Myrange:
+    def __init__(self, path):
+        self.index = -1
+        with open(path, "r") as read_file:
+            self.data = json.load(read_file)
+            self.end = len(self.data)
+    def __iter__(self):
+        return self
+    def __next__(self):
+        self.index += 1
+        if self.index >= self.end:
+            raise StopIteration
+        name = self.data[self.index]['name']['common']
+        new_name = name.replace(' ', '_')
+        return WIKI + new_name
 
 if __name__ == '__main__':
-    for i in gen('countries.txt'):
+    for i in Myrange('countries.json'):
         print(i)
